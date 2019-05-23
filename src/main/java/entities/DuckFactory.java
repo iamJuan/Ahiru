3975fa2c94f2eb8c7aad3d6b5e81fdba954fe331
iAmJuan <john_aparejado@yahoo.com>
2019-05-23 23:23:51 +0800

    refactor WorldCreation

---
+++ src/main/java/entities/DuckFactory.java
@@ -0,0 +1,27 @@
+package entities;
+
+import static utilities.EntityConstants.*;
+
+public class DuckFactory {
+
+    public static Duck getDuck(String duckType, int xPos, int lane){
+        Duck duck = null;
+        if (EASY_DUCK.equalsIgnoreCase(duckType)){
+            duck = new EasyDuck(xPos, lane);
+        }else if(HELM_DUCK.equalsIgnoreCase(duckType)){
+            duck = new HelmDuck(xPos, lane);
+        }else if(KNIGHT_DUCK.equalsIgnoreCase(duckType)){
+            duck = new KnightDuck(xPos, lane);
+        }else if(CLOWN_DUCK.equalsIgnoreCase(duckType)){
+            duck = new ClownDuck(xPos, lane);
+        }else{
+            try {
+                throw new Exception();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return duck;
+    }
+}
---
+++ src/main/java/utilities/EntityConstants.java
@@ -0,0 +1,12 @@
+package utilities;
+
+public class EntityConstants {
+    public static final String EASY_DUCK = "easy";
+    public static final String HELM_DUCK = "helm";
+    public static final String KNIGHT_DUCK = "knight";
+    public static final String CLOWN_DUCK = "clown";
+    public static final int EASY = 1;
+    public static final int HELM = 2;
+    public static final int KNIGHT = 3;
+    public static final int CLOWN = 4;
+}
